0733d96badda64bf8ce53460a502897f0f51baa6
Charles Karari <kararicw@yahoo.com>
2020-08-25 23:44:19 +0300

    changes to html and js

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,25 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'CK Name Generator';
 
 var bday;
-bday = window.prompt("When is your birthday (YYYY-MM-DD)?","");
+bday = window.prompt("When is your birthday (YYYY-MM-DD)?", "");
 var bdayArray = bday.split('-')
 
-if(bdayArray.length !== 3){
-     alert("invalid Date");
-}else{
-    if(!bdayArray[0].match(/^\d\d\d\d$/) || 
-       !bdayArray[1].match(/^\d\d$/) || 
-       !bdayArray[2].match(/^\d\d$/)){
-        alert("invalid Date");    
-    }else{
-        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];     
-        var currentTime = new Date(            
-            parseInt(bdayArray[0]),
-            parseInt(bdayArray[1]) - 1, //month starts from 0
-            parseInt(bdayArray[2])
-        );        
-        var currentDay = currentTime.getDay();       
-        var currentDayName= days[currentDay];
-       document.write("You were born on "+currentDayName);
-    }
+if (bdayArray.length !== 3) {
+   alert("invalid Date");
+} else {
+   if (!bdayArray[0].match(/^\d\d\d\d$/) ||
+      !bdayArray[1].match(/^\d\d$/) ||
+      !bdayArray[2].match(/^\d\d$/)) {
+      alert("invalid Date");
+   } else {
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var currentTime = new Date(
+         parseInt(bdayArray[0]),
+         parseInt(bdayArray[1]) - 1, //month starts from 0
+         parseInt(bdayArray[2])
+      );
+      var currentDay = currentTime.getDay();
+      var currentDayName = days[currentDay];
+      document.write("You were born on " + currentDayName);
+   }
 }
