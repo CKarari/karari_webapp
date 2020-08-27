8d684b3b5c03bb5a62d3772b0e5f05b25a47e282
Charles Karari <kararicw@yahoo.com>
2020-08-28 00:50:50 +0300

    revised scripts and styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,72 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'CK Name Generator';
+let akanForm = document.getElementById("akanForm");
+akanForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    gender = document.getElementById("gender").value
+    dateOfBirth = document.getElementById("date").value
+    
+    dateOfBirth = new Date(dateOfBirth);
+    
+    let yearOfBirth = dateOfBirth.getFullYear();
+    let birthMonth = dateOfBirth.getMonth() + 1;
+    let dayOfBirth = dateOfBirth.getDate();
+    
+     CC = yearOfBirth.toString().substr(0, 2);
+    YY = yearOfBirth.toString().substr(2, 2);
+    MM = birthMonth
+    DD = dayOfBirth
+    
+    dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+    dayOfTheWeek = parseInt(dayOfTheWeek);
+    
+    let akanName;
+    femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
+    maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
+    let bornDay;
+    if (dayOfTheWeek == 0 && gender == "female") {
+        bornDay = "Sunday";
+        akanName = femaleAkanNames[0];
+    } else if (dayOfTheWeek == 0 && gender == "male") {
+        bornDay = "Sunday";
+        akanName = maleAkanNames[0];
+    } else if (dayOfTheWeek == 1 && gender == "female") {
+        bornDay = "Monday";
+        akanName = femaleAkanNames[1];
+    } else if (dayOfTheWeek == 1 && gender == "male") {
+        bornDay = "Monday";
+        akanName = maleAkanNames[1];
+    } else if (dayOfTheWeek == 2 && gender == "female") {
+        bornDay = "Tuesday";
+        akanName = femaleAkanNames[2];
+    } else if (dayOfTheWeek == 2 && gender == "male") {
+        bornDay = "Tuesday";
+        akanName = maleAkanNames[2];
+    } else if (dayOfTheWeek == 3 && gender == "female") {
+        bornDay = "Wednesday";
+        akanName = femaleAkanNames[3];
+    } else if (dayOfTheWeek == 3 && gender == "male") {
+        bornDay = "Wednesday";
+        akanName = maleAkanNames[3];
+    } else if (dayOfTheWeek == 4 && gender == "female") {
+        bornDay = "Thursday";
+        akanName = femaleAkanNames[4];
+    } else if (dayOfTheWeek == 4 && gender == "male") {
+        bornDay = "Thursday";
+        akanName = maleAkanNames[4];
+    } else if (dayOfTheWeek == 5 && gender == "female") {
+        bornDay = "Friday";
+        akanName = femaleAkanNames[5];
+    } else if (dayOfTheWeek == 5 && gender == "male") {
+        bornDay = "Friday";
+        akanName = maleAkanNames[5];
+    } else if (dayOfTheWeek == 6 && gender == "female") {
+        bornDay = "Saturday";
+        akanName = femaleAkanNames[6];
+    } else if (dayOfTheWeek == 6 && gender == "male") {
+        bornDay = "Saturday";
+        akanName = maleAkanNames[6];
+    }
 
-function MakeArray(n) {
-   this.length = n;
-   for (var i = 1; i <=n; i++) {
-     this[i] = 0;
-   }
-}
-
-
-days = new MakeArray(7);
-days[0] = "Sunday"
-days[1] = "Monday"
-days[2] = "Tuesday"
-days[3] = "Wednesday"
-days[4] = "Thursday"
-days[5] = "Friday"
-days[6] = "Saturday"
-
-
-months = new MakeArray(12);
-months[1] = "January" 
-months[2] = "February" 
-months[3] = "March" 
-months[4] = "April" 
-months[5] = "May" 
-months[6] = "June" 
-months[7] = "July" 
-months[8] = "August"
-months[9] = "September" 
-months[10] = "October"
-months[11] = "November"
-months[12] = "December"
-
-
-function compute(form) {
-   var val1 = parseInt(form.day.value,  10)
-   if ((val1 < 0) || (val1 > 31)) {
-      alert("Day is out of range")
-   }
-   var val2 = parseInt(form.month.value, 10)
-   if ((val2 < 0) || (val2 > 12)) {
-      alert("Month is out of range")
-   }  
-   var val2x = parseInt(form.month.value, 10)
-   var val3 = parseInt(form.year.value, 10)
-   if (val3 < 1900) {
-      alert("Really!")
-   }
-   if (val2 == 1) {
-      val2x = 13;
-      val3 = val3-1
-   }
-   if (val2 == 2) {
-      val2x = 14;
-      val3 = val3-1
-   }
-   var val4 = parseInt(((val2x+1)*3)/5, 10)
-   var val5 = parseInt(val3/4, 10)
-   var val6 = parseInt(val3/100, 10)
-   var val7 = parseInt(val3/400, 10)
-   var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-   var val9 = parseInt(val8/7, 10)
-   var val0 = val8-(val9*7)
-   form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-   form.result2.value = days[val0]
-
-}
+    let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
+    document.getElementById("akanForm").reset();
+});
