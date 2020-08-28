ff8ebf00a35a3e13c99f1b4e82ac0eeb014efecc
Charles Karari <kararicw@yahoo.com>
2020-08-28 19:46:47 +0300

    revision to main.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## CK Name Generator
+## CK Akan Name Generator
 
 ## Description
 This is a name generator app that generates name based on sex and the day of the week that one was born. It is developed to support an age old culture in Ghana of naming children based on the day of the week in which they were born. The app quickly computes the day of birth based on the date and year of birth. It then suggests the name based on the sex of the child and the day of the week in which the child was born
--- scripts/main.js
+++ scripts/main.js
@@ -1,72 +1,91 @@
-let akanForm = document.getElementById("akanForm");
-akanForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    gender = document.getElementById("gender").value
-    dateOfBirth = document.getElementById("date").value
-    
-    dateOfBirth = new Date(dateOfBirth);
-    
-    let yearOfBirth = dateOfBirth.getFullYear();
-    let birthMonth = dateOfBirth.getMonth() + 1;
-    let dayOfBirth = dateOfBirth.getDate();
-    
-     CC = yearOfBirth.toString().substr(0, 2);
-    YY = yearOfBirth.toString().substr(2, 2);
-    MM = birthMonth
-    DD = dayOfBirth
-    
-    dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-    dayOfTheWeek = parseInt(dayOfTheWeek);
-    
-    let akanName;
-    femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
-    maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
-    let bornDay;
-    if (dayOfTheWeek == 0 && gender == "female") {
-        bornDay = "Sunday";
-        akanName = femaleAkanNames[0];
-    } else if (dayOfTheWeek == 0 && gender == "male") {
-        bornDay = "Sunday";
-        akanName = maleAkanNames[0];
-    } else if (dayOfTheWeek == 1 && gender == "female") {
-        bornDay = "Monday";
-        akanName = femaleAkanNames[1];
-    } else if (dayOfTheWeek == 1 && gender == "male") {
-        bornDay = "Monday";
-        akanName = maleAkanNames[1];
-    } else if (dayOfTheWeek == 2 && gender == "female") {
-        bornDay = "Tuesday";
-        akanName = femaleAkanNames[2];
-    } else if (dayOfTheWeek == 2 && gender == "male") {
-        bornDay = "Tuesday";
-        akanName = maleAkanNames[2];
-    } else if (dayOfTheWeek == 3 && gender == "female") {
-        bornDay = "Wednesday";
-        akanName = femaleAkanNames[3];
-    } else if (dayOfTheWeek == 3 && gender == "male") {
-        bornDay = "Wednesday";
-        akanName = maleAkanNames[3];
-    } else if (dayOfTheWeek == 4 && gender == "female") {
-        bornDay = "Thursday";
-        akanName = femaleAkanNames[4];
-    } else if (dayOfTheWeek == 4 && gender == "male") {
-        bornDay = "Thursday";
-        akanName = maleAkanNames[4];
-    } else if (dayOfTheWeek == 5 && gender == "female") {
-        bornDay = "Friday";
-        akanName = femaleAkanNames[5];
-    } else if (dayOfTheWeek == 5 && gender == "male") {
-        bornDay = "Friday";
-        akanName = maleAkanNames[5];
-    } else if (dayOfTheWeek == 6 && gender == "female") {
-        bornDay = "Saturday";
-        akanName = femaleAkanNames[6];
-    } else if (dayOfTheWeek == 6 && gender == "male") {
-        bornDay = "Saturday";
-        akanName = maleAkanNames[6];
-    }
+function generateAkanname(){
 
-    let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
-    document.getElementById("akanForm").reset();
-});
+    // let akanForm = document.getElementById("akanForm");
+    // akanForm.addEventListener("submit", (e) => {
+    //     e.preventDefault();
+        
+        femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
+        maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
+
+       var gender = document.getElementsByName("gender")
+        var dateOfBirth = document.getElementById("date").value
+        
+        days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+       
+        var date = new Date(dateOfBirth);
+       var dayOfTheWeek = date.getDay();
+       
+       
+       for (var i = 0; i < gender.length; i++) {
+           if (gender[i].checked) {
+              if (gender[i].value === "male") {
+                  document.getElementById("reveal-result").innerHTML="born on a" + days[dayOfTheWeek] + "Your Akan Name" +maleAkanNames[dayOfTheWeek] 
+              }
+              else {
+                  document.getElementById("reveal-result").innerHTML= "born on a" + days[dayOfTheWeek] + "Your Akan Name" + femaleAkanNames[dayOfTheWeek] 
+              }
+           }
+           
+           
+           
+       }
+        
+        
+//         let akanName;
+//         
+//         let bornDay;
+//         if (dayOfTheWeek == 0 && gender == "female") {
+//             bornDay = "Sunday";
+//             akanName = femaleAkanNames[0];
+//         } else if (dayOfTheWeek == 0 && gender == "male") {
+//             bornDay = "Sunday";
+//             akanName = maleAkanNames[0];
+//         } else if (dayOfTheWeek == 1 && gender == "female") {
+//             bornDay = "Monday";
+//             akanName = femaleAkanNames[1];
+//         } else if (dayOfTheWeek == 1 && gender == "male") {
+//             bornDay = "Monday";
+//             akanName = maleAkanNames[1];
+//         } else if (dayOfTheWeek == 2 && gender == "female") {
+//             bornDay = "Tuesday";
+//             akanName = femaleAkanNames[2];
+//         } else if (dayOfTheWeek == 2 && gender == "male") {
+//             bornDay = "Tuesday";
+//             akanName = maleAkanNames[2];
+//         } else if (dayOfTheWeek == 3 && gender == "female") {
+//             bornDay = "Wednesday";
+//             akanName = femaleAkanNames[3];
+//         } else if (dayOfTheWeek == 3 && gender == "male") {
+//             bornDay = "Wednesday";
+//             akanName = maleAkanNames[3];
+//         } else if (dayOfTheWeek == 4 && gender == "female") {
+//             bornDay = "Thursday";
+//             akanName = femaleAkanNames[4];
+//         } else if (dayOfTheWeek == 4 && gender == "male") {
+//             bornDay = "Thursday";
+//             akanName = maleAkanNames[4];
+//         } else if (dayOfTheWeek == 5 && gender == "female") {
+//             bornDay = "Friday";
+//             akanName = femaleAkanNames[5];
+//         } else if (dayOfTheWeek == 5 && gender == "male") {
+//             bornDay = "Friday";
+//             akanName = maleAkanNames[5];
+//         } else if (dayOfTheWeek == 6 && gender == "female") {
+//             bornDay = "Saturday";
+//             akanName = femaleAkanNames[6];
+//         } else if (dayOfTheWeek == 6 && gender == "male") {
+//             bornDay = "Saturday";
+//             akanName = maleAkanNames[6];
+//         }
+    
+//         let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
+//         document.getElementById("akanForm").reset();
+  
+    
+var form = document.getElementById("akanForm")
+function handleForm(event) {
+ event.preventDefault();   
+}
+form.addEventListener("submit", handleForm);
+};
+
